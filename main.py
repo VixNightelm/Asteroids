e5ff9b377d78c5cdd9cfaac5b4bcc03391ad98e9
VixNightelm <kinykchristian@gmail.com>
2025-10-10 08:52:47 -0500

    Added a tick clock to main.py to limit frame rate

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from constants import *
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	dt = 0
+	clock = pygame.time.Clock()
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+		ms = clock.tick(60)
+		dt = ms / 1000
+		print(dt)
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
