b883cbf77e282e1fb18cb6324a30f9dff049fd63
VixNightelm <kinykchristian@gmail.com>
2025-10-13 08:48:14 -0500

    Added player.py and circleshape.py to draw player to screen; added player rotation

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	dt = 0
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
 	clock = pygame.time.Clock()
+	player = Player(x, y)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
+		player.draw(screen)
+		player.update(dt)
 		pygame.display.flip()
 		ms = clock.tick(60)
 		dt = ms / 1000
-		print(dt)
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
