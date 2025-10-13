3f0d0f1b4f017b71e7636225d32137cde7e18f70
VixNightelm <kinykchristian@gmail.com>
2025-10-13 09:19:09 -0500

    Added asteroids and asteroid field to draw and move asteroids on screen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
 	pygame.init()
@@ -8,15 +11,23 @@ def main():
 	dt = 0
 	x = SCREEN_WIDTH / 2
 	y = SCREEN_HEIGHT / 2
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 	clock = pygame.time.Clock()
+	field = AsteroidField()
 	player = Player(x, y)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
-		player.draw(screen)
-		player.update(dt)
+		for sprite in drawable:
+			sprite.draw(screen)
+		updatable.update(dt)
 		pygame.display.flip()
 		ms = clock.tick(60)
 		dt = ms / 1000
