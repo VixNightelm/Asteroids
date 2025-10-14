3fec544bcef49b4222c35f953282a42e2175cd31
VixNightelm <kinykchristian@gmail.com>
2025-10-14 12:12:47 -0500

    Added duration to bullets so they are no longer infinite

--- shot.py
+++ shot.py
@@ -6,9 +6,13 @@ from constants import *
 class Shot(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, SHOT_RADIUS)
+		self.duration = 1
 
 	def draw(self, screen):
 		pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
 
 	def update(self, dt):
 		self.position += self.velocity * dt
+		self.duration -= dt
+		if self.duration <= 0:
+			self.kill()
